a6dc48ced86bac309fe02eca12e687cd7e75edc5
Dmitry Noranovich <javaeeeee@gmail.com>
2017-02-25 15:38:05 -0500

    Updated Configurtion

--- SpringREST/src/main/java/com/javaeeeee/springrest/controllers/HelloController.java
+++ SpringREST/src/main/java/com/javaeeeee/springrest/controllers/HelloController.java
@@ -23,7 +23,7 @@
  */
 package com.javaeeeee.springrest.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -33,7 +33,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String getGreeting() {
         return "Hello, World";
     }
